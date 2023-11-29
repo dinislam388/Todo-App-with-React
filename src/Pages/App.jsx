eea5190069355479226b8961717433b2cc14eeb0
dinislam388 <dinislam01319950749@gmail.com>
2023-11-29 22:36:34 +0600

    Done

---
+++ src/Pages/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import TodoApp from '../Components/TodoApp/TodoApp'
+// import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <TodoApp/>
+      </div>
+    </>
+  )
+}
+
+export default App
